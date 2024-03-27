7d71bea7d512c492d8b95a57b0cd00bb7148a33e
TZaidel <8tzaidel@gmail.com>
2024-03-27 14:51:39 +0100

    fix removeContact

--- contacts.js
+++ contacts.js
@@ -22,7 +22,7 @@ async function removeContact(contactId) {
   if (index === -1) {
     return null
   }
-  const result = contacts.splice(index, 1)
+  const [result] = contacts.splice(index, 1)
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
   return result
 }
